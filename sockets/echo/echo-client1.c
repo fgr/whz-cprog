16281a47878e2573499cbb8486d47b2f76eff267
Frank Grimm <grimmfrank@googlemail.com>
2015-03-17 20:41:55 +0100

    First version of the echo client/server.

---
+++ sockets/echo/echo-client1.c
@@ -0,0 +1,38 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+// Ausbaustufe 1: Verbindung zum Server aufbauen
+int main(void) {
+  // Creates an endpoint for communication and returns a descriptor.
+  // AF_INET fuer IPv4-Verbindung
+  // SOCK_STREAM und 0 fuer TCP (Provides sequenced, reliable, two-way,
+  // connection-based byte streams.)
+  const int client_sock = socket(AF_INET, SOCK_STREAM, 0);
+
+  // Daten des Server-Kommunikationsendpunktes initialiseren
+  struct sockaddr_in server_addr;
+  // Alle Felder ausnullen
+  memset(&server_addr, 0, sizeof(server_addr));
+  // IPv4-Verbindung
+  server_addr.sin_family = AF_INET;
+  // IP-String zu binaerer IPv4-Adresse
+  inet_pton(AF_INET, "127.0.0.1", &(server_addr.sin_addr));
+  // Server akzeptiert Verbindungen an TCP-Port 22000
+  server_addr.sin_port = htons(22000);
+
+  // Verbindung herstellen
+  connect(client_sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  printf("Connection established; server %s:%i\n",
+         inet_ntoa(server_addr.sin_addr), server_addr.sin_port);
+
+  // Auf Tastendruck warten
+  getchar();
+
+  // Verbindung trennen
+  close(client_sock);
+  printf("Connection closed.\n");
+}
---
+++ sockets/echo/echo-server1.c
@@ -0,0 +1,48 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+// Ausbaustufe 1: Verbindungen von Clients annehmen (nur eine Verbindung
+// gleichzeitig moeglich)
+int main(void) {
+  // Creates an endpoint for communication and returns a descriptor.
+  // - AF_INET fuer IPv4-Verbindung
+  // - SOCK_STREAM und 0 fuer TCP (Provides sequenced, reliable, two-way,
+  //   connection-based byte streams.)
+  const int server_sock = socket(AF_INET, SOCK_STREAM, 0);
+
+  // Daten des Server-Kommunikationsendpunktes initialiseren
+  struct sockaddr_in server_addr;
+  // Alle Felder ausnullen
+  memset(&server_addr, 0, sizeof(server_addr));
+  // IPv4-Verbindung
+  server_addr.sin_family = AF_INET;
+  // Auf allen Netzwerk-Interfaces auf Verbindungen warten
+  server_addr.sin_addr.s_addr = htons(INADDR_ANY);
+  // Auf Verbindungen an TCP-Port 22000 warten
+  server_addr.sin_port = htons(22000);
+
+  // assigns the address specified by serv_addr to the socket referred to by the
+  // file descriptor server_sock
+  bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
+
+  printf("Waiting for connections from clients...\n");
+
+  // listen for connections on a socket. (blockierendes Warten)
+  // marks the socket referred to by sockfd as a passive socket, that is, as a
+  // socket that will be used to accept incoming connection requests using
+  // accept.
+  // - 10 = maximum length to which the queue of pending connections
+  listen(server_sock, 10);
+
+  const int client_sock = accept(server_sock, NULL, NULL);
+
+  printf("Connection established.\n");
+
+  getchar();
+  close(client_sock);
+  printf("Connection closed.\n");
+}
